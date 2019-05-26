e9f6044197ed9f186381353f6f25b9325dcf2b16
dlwl0319 <dlwl0391@gmail.com>
2019-05-26 09:29:41 +0000

    [templates] applied css to the templates and make navbar, [userAPI.py] change some code...

--- app/view/usersAPI.py
+++ app/view/usersAPI.py
@@ -10,36 +10,50 @@ usersAPI = Blueprint('usersAPI', __name__, template_folder='templates')
 def signup():
 	if request.method == 'GET':
 		if 'userEmail' in session:
-			return render_template('welcome.html', info = session['userEmail'])
+			info = session['userEmail'].split('@')
+			return render_template('welcome.html', info = info[0])
 		else:
 			return render_template('signup.html')
 	
 	if request.method == 'POST':
 		if 'userEmail' in session:	
-			return render_template('welcome.html', info = session['userEmail'])
+			info = session['userEmail'].split('@')
+			return render_template('welcome.html', info = info[0])
 		else:
 			users.userCreate(request.form.to_dict(flat='true'))
 			session['userEmail'] = request.form['userEmail']
-			return render_template('welcome.html', info = session['userEmail'])
+			info = session['userEmail'].split('@')
+			return render_template('welcome.html', info = info[0])
 
 @usersAPI.route('/')
+def base():
+	if 'userEmail' in session:
+		info = session['userEmail'].split('@')
+		return render_template('welcome.html', info = info[0])
+	return render_template('welcome.html')
+
 @usersAPI.route('/signin', methods=['GET', 'POST'])
 def signin():
 	if request.method == 'GET':
 		if 'userEmail' in session:
-			return render_template('welcome.html', info = session['userEmail'])
+			info = session['userEmail'].split('@')
+			return render_template('welcome.html', info = info[0])
 		else:
 			return render_template('signin.html')
 
 	if request.method == 'POST':
 		if 'userEmail' in session:
-			return render_template('welcome.html', info = session['userEmail'])
+			info = session['userEmail'].split('@')
+			return render_template('welcome.html', info = info[0])
 		else:
 			users.userAuthentication(request.form.to_dict(flat='true'))
 			session['userEmail'] = request.form['userEmail']
-			return render_template('welcome.html', info = session['userEmail'])
+			info = session['userEmail'].split('@')
+			return render_template('welcome.html', info = info[0])
+
 @usersAPI.route('/logout')
 def logout():
 	if 'userEmail' in session:
 		session.pop('userEmail')
 		return redirect(url_for('usersAPI.signin'))
+	return redirect(url_for('usersAPI.signin'))
