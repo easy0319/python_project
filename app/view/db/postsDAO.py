7f9818c5edf2a09980e3ef21a4f52fe3d1c93fca
dlwl0319 <dlwl0391@gmail.com>
2019-06-13 04:21:44 +0000

    change post model, make edit profile, make post update & delete & more

--- app/view/db/postsDAO.py
+++ app/view/db/postsDAO.py
@@ -1,27 +1,35 @@
 import pymongo
+from bson.objectid import ObjectId
 
 class Posts():
-    def __init__(self, db):
-        self.posts = pymongo.collection.Collection(db, 'Posts')
-    
-    def postValidation(self):
-        try:
-            posts = self.posts.find()
-            return posts
-        except:
-            return False
+	def __init__(self,db):
+		self.posts = pymongo.collection.Collection(db, 'Posts')
 
-    def postCreate(self, postDict):
-        try:
-            posts = self.posts.insert_one(postDict)
-            return posts
-        except:
-            return False
+	def postCreate(self,postDict):
+		try:
+			obj_id = self.posts.insert_one(postDict).inserted_id
+			return obj_id
+		except:
+			return False
 
-    def postAuthentication(self, postDict):
-        if self.posts.find_one(postDict) is not None:
-            try:
-                posts = self.posts.find_one(postDict)
-                return posts
-            except:
-                return False
+	def postDelete(self,obj_id):
+		try:
+			self.posts.delete_one({"_id":ObjectId(obj_id)})
+			return True
+		except:
+			return False
+
+	def postUpdate(self,postDict):
+		self.posts.find_and_modify(
+			{"_id": ObjectId(postDict["obj_id"])},
+			{"$set":{"postTitle":postDict["postTitle"],"postContent":postDict["postContent"]}},
+			upsert=False
+			)
+		return True
+
+	def getAllposts(self):
+		try:
+			result = self.posts.find({})
+			return result
+		except:
+			return False
--- app/view/postsAPI.py
+++ app/view/postsAPI.py
@@ -1,60 +1,59 @@
 from flask import Blueprint, request, render_template, session, redirect, url_for
-from .db import connect_mongo, postsDAO
+from .db import connect_mongo, postsDAO, profileDAO
+import time
+
+def dict_merge(x, y):
+    return {**x, **y}
 
 db_connection = connect_mongo.ConnectDB().db
 posts = postsDAO.Posts(db_connection)
+profile = profileDAO.Profile(db_connection)
 
 postsAPI = Blueprint('postsAPI', __name__, template_folder='templates')
-
-@postsAPI.route('/post')
-def post():
-    if 'userEmail' in session:
-        info = session['userEmail'].split('@')
-        return render_template('post.html', info = info[0])
-    else:
-        post = posts.postValidation()
-        return render_template('welcome.html', post = post)
 
 @postsAPI.route('/', methods=['GET'])
 def base():
     if request.method == 'GET':
         if 'userEmail' in session:
-            post = posts.postValidation()
+            post = posts.getAllposts()
+            pro = profile.profileValidation()
             info = session['userEmail'].split('@')
-            return render_template('welcome.html', info = info[0], post = post)
+            return render_template('welcome.html', info = info[0], post = post, profile = pro)
         else:
-            post = posts.postValidation()
-            return render_template('welcome.html', post = post)
+            post = posts.getAllposts()
+            pro = profile.profileValidation()
+            return render_template('welcome.html', post = post, profile = pro)
 
 @postsAPI.route('/posting', methods=['GET', 'POST'])
 def posting():
     if request.method == 'GET':
-        if 'userEmail' in session:
-            post = posts.postValidation()
+        if 'userEmail' in session and session['userEmail'] == 'admin@admin':
             info = session['userEmail'].split('@')
-            return render_template('welcome.html', info = info[0], post = post)
+            return render_template('post.html', info = info[0])
         else:
-            post = posts.postValidation()
-            return render_template('welcome.html', post = post)
+            return redirect(url_for('postsAPI.base'))
 
     if request.method == 'POST':
-        if 'userEmail' in session:
-                posts.postCreate(request.form.to_dict(flat='true'))
-                post = posts.postValidation()
-                info = session['userEmail'].split('@')
-                return render_template('welcome.html', info = info[0], post = post)
+        if 'userEmail' in session and session['userEmail'] == 'admin@admin':
+            now = time.strftime("%Y-%m-%d %H:%M")
+            obj_id = posts.postCreate(dict_merge({"author":session['userEmail'], "date":now}, request.form.to_dict(flat=True)))
+            return redirect(url_for('postsAPI.base'))
         else:
-            post = posts.postValidation()
-            return render_template('welcome.html', post = post)
+            return redirect(url_for('postsAPI.base'))
 
-@postsAPI.route('/postview', methods=['GET'])
-def postview():
-    if 'userEmail' in session:
-        info = session['userEmail'].split('@')
-        post = posts.postAuthentication(request.form.to_dict(flat='true'))
-        return render_template('postview.html', info = info[0], post = post)
+@postsAPI.route('/post/update', methods=['POST'])
+def postUpdate():
+    if 'userEmail' in session and session['userEmail'] == 'admin@admin':
+        posts.postUpdate(request.form.to_dict(flat=True))
+        return redirect(url_for('postsAPI.base'))
     else:
-        post = posts.postValidation()
-        return render_template('welcome.html', post = post)
+        return redirect(url_for('postsAPI.base'))
 
 
+@postsAPI.route('/post/delete', methods=['POST'])
+def postDelete():
+    if 'userEmail' in session and session['userEmail'] == 'admin@admin':
+        posts.postDelete(request.form.to_dict(flat=True)["obj_id"])
+        return redirect(url_for('postsAPI.base'))
+    else:
+        return redirect(url_for('postsAPI.base'))
